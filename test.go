57ec75efc5702ad905f99516e0750eda2ca8f9ea
hikalium <hikalium@hikalium.com>
2022-11-23 16:04:15 +0900

    update

--- test.go
+++ test.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/binary"
 	"encoding/hex"
 	"fmt"
 	"log"
 	"net"
+	"time"
 )
 
 type TestConfig struct {
@@ -12,33 +14,23 @@ type TestConfig struct {
 }
 
 func getConfig() (*TestConfig, error) {
-	/*
-		raw, err := ioutil.ReadFile("test.config.json")
-		if err != nil {
-			return nil, err
-		}
-		config := &TestConfig{}
-		err = json.Unmarshal(raw, config)
-		if err != nil {
-			return nil, err
-		}
-	*/
 	return &TestConfig{
 		RemoteIpAddr: "10.10.10.31",
 	}, nil
 }
 
 var requestDefinitions = map[string][]byte{
-	"get_tape_info": {
+	"get_printer_status": {
 		0x54, 0x50, 0x52, 0x54, 0x00, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x20,
 		0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 	},
-	// 00000000  74 70 72 74 00 00 00 00  00 00 00 01 00 00 00 20  |tprt........... |
-	// 00000010  00 00 00 01 00 00 00 14  00 00 00 00 00 00 00 00  |................|
-	// 00000020  14 00 00 05 00 00 00 00  40 00 00 00 00 01 00 00  |........@.......|
-	// 00000030  00 00 00 00                                       |....|
+	// 00000000  74 70 72 74 00 00 00 00  00 00 00 01 00 00 00 20
+	// 00000010  00 00 00 01 00 00 00 14  [IP       ] [?        ]
+	// 00000020  14 00 00 XX 00 00 00 00  40 00 00 00 00 YY 00 00
+	// 00000030  00 00 00 00
+	// YY@+0x2d 00: printing, 01: done?
 
 	"print_start": {
 		0x54, 0x50, 0x52, 0x54, 0x00, 0x00, 0x00, 0x00,
@@ -50,7 +42,7 @@ var requestDefinitions = map[string][]byte{
 	// 00000010  00 00 00 02 00 00 00 03  00 00 00 00 00 00 00 00  |................|
 	// 00000020  02 00 00
 
-	"request03": {
+	"print_stop": {
 		0x54, 0x50, 0x52, 0x54, 0x00, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x20,
 		0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00,
@@ -100,54 +92,51 @@ func testUDPMessage(config *TestConfig, key string) []byte {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s", hex.Dump(buffer[:length]))
-	//fmt.Println(hex.EncodeToString(buffer[:length]))
+	// fmt.Printf("%s", hex.Dump(buffer[:length]))
 	return buffer[:length]
 }
 
-// [header (66 bytes)] ([separator (10 bytes)][data(34 bytes)])*435(0x1b3) [terminator(7 bytes)]
-// 19213 = 66 + 44 * 435 + 7
-
-// header
-// 1b7b 07 7b00005354227d
-// 1b7b 07 4302020101497d
-// 1b7b 04 4405497d
-// 1b7b 03 47477d
-// 1b7b 04 7300737d
-// 1b7b 05 6c0505767d
-// 1b7b 07 4cb3010000007d
-// 1b7b 05 549b00ef7d
-
-// separator (10 bytes)
-// 1b 2e 00 00 00 01 1d 01 00 00
-// 34 bytes
-// 44 bytes for one line (height) when 24mm tape
-// 2 byte per 1mm?
-
-// terminator (7 bytes)
-//		0c 1b 7b 03 40407d
-
-var header = []byte{
-	0x1b, 0x7b, 0x07, 0x43, 0x02, 0x02, 0x01, 0x01, 0x49, 0x7d,
-	0x1b, 0x7b, 0x04, 0x44, 0x05, 0x49, 0x7d,
-	0x1b, 0x7b, 0x05, 0x6c, 0x05, 0x05, 0x76, 0x7d,
-	0x1b, 0x7b, 0x04, 0x73, 0x00, 0x73, 0x7d,
-	0x1b, 0x7b, 0x03, 0x47, 0x47, 0x7d,
-	0x1b, 0x7b, 0x07, 0x4c, 0xe2, 0x00, 0x00, 0x00, 0x2e, 0x7d, // [4..8]: len?
+var header_common = []byte{
+	0x1b, 0x7b, 0x07, 0x43, 0x02, 0x02, 0x01, 0x01, 0x49, 0x7d, // fixed
+	0x1b, 0x7b, 0x03, 0x47, 0x47, 0x7d, // fixed
+	0x1b, 0x7b, 0x04, 0x44, 0x05, 0x49, 0x7d, // fixed
+	0x1b, 0x7b, 0x04, 0x73, 0x00, 0x73, 0x7d, // fixed
 	0x1b, 0x7b, 0x05, 0x54, 0x40, 0x00, 0x94, 0x7d,
+	0x1b, 0x7b, 0x05, 0x6c, 0x05, 0x05, 0x76, 0x7d, // fixed
+	0x1b, 0x7b, 0x07, 0x7b, 0x00, 0x00, 0x53, 0x54, 0x22, 0x7d, // frame square? -> maybe not...
 }
+var header_print_size = []byte{
+	0x1b, 0x7b, 0x07, 0x4c, 0xe2, 0x00, 0x00, 0x00, 0x2e, 0x7d, // 4c=data size, [4..8]: len?
+}
+
+// 360dpi =>
+// px = mm/25.4*360
+// mm = px/360*25.4
+
+// 384dot =>
+// 48*8 bits => 48 bytes per row is the max
 
 var header_per_line = []byte{
 	0x1b, 0x2e, 0x00, 0x0a, 0x0a, 0x01, 0x90, 0x00,
 }
+var termination = []byte{
+	0x0c,
+	0x1b, 0x7b, 0x03, 0x40, 0x40, 0x7d,
+}
+
+// 18mm : 1b2e 0000 0001 d700
+// 12mm : 1b2e 000a 0a0a 9000
 
 func testPrint(config *TestConfig) error {
-	if len(header) != 56 {
-		return fmt.Errorf("Header length MUST be 56 Bytes")
-	}
-	message_body := header
-	for y := 0; y < 0xe2; y++ {
-		content_line := make([]byte, 34)
+	len_mm := 45.0
+	len_px := int(len_mm * 360.0 / 25.4)
+	log.Printf("len_px = 0x%08x", len_px)
+	message_body := header_common
+	binary.LittleEndian.PutUint32(header_print_size[4:], uint32(len_px))
+	message_body = append(message_body, header_print_size...)
+	fmt.Printf("%s", hex.Dump(message_body))
+	for y := 0; y < len_px; y++ {
+		content_line := make([]byte, 40)
 		for i := 0; i < len(content_line); i++ {
 			chunk := 0
 			for k := 0; k < 8; k++ {
@@ -162,9 +151,6 @@ func testPrint(config *TestConfig) error {
 		}
 		line := append(header_per_line, content_line...)
 		message_body = append(message_body, line...)
-	}
-	termination := []byte{
-		0x0c, 0x1b, 0x7b, 0x03, 0x40, 0x40, 0x7d,
 	}
 	message_body = append(message_body, termination...)
 	conn, err := net.Dial("tcp4", config.RemoteIpAddr+":9100")
@@ -182,10 +168,21 @@ func testPrint(config *TestConfig) error {
 func do_print(config *TestConfig) {
 	testUDPMessage(config, "print_start")
 	testPrint(config)
+	for {
+		time.Sleep(500 * time.Millisecond)
+		res := testUDPMessage(config, "get_printer_status")
+		if res[0x2d] == 0 {
+			log.Print("waiting...\n")
+			continue
+		}
+		log.Printf("%02x\n", res[0x2d])
+		break
+	}
+	testUDPMessage(config, "print_stop")
 }
 
 func get_tape_width(config *TestConfig) {
-	res := testUDPMessage(config, "get_tape_info")
+	res := testUDPMessage(config, "get_printer_status")
 	expected := []byte{
 		0x74, 0x70, 0x72, 0x74, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x20,
 		0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x14, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -194,7 +191,7 @@ func get_tape_width(config *TestConfig) {
 	}
 	fmt.Printf("%s", hex.Dump(res))
 	for i := 0; i < len(expected); i++ {
-		if i == 0x22 || i == 0x23 || i == 0x25 {
+		if i == 0x22 || i == 0x23 || i == 0x25 || i == 0x2d {
 			continue
 		}
 		if res[i] != expected[i] {
@@ -221,6 +218,9 @@ func get_tape_width(config *TestConfig) {
 		log.Fatalf("Unexpected byte at index 0x%02x: expected 0x00 or 0x06 but got 0x%02x", index, res[index])
 	}
 
+	index = 0x2d
+	log.Printf("res[0x%02x] = 0x%02x", index, res[index])
+
 	index = 0x23
 	tape_index := res[index]
 	if tape_index == 0x01 {
@@ -245,6 +245,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	//do_print(config)
 	get_tape_width(config)
+	do_print(config)
 }
