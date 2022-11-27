08a65ca3d0e67ba028e94535192f8d965140b6f2
hikalium <hikalium@hikalium.com>
2022-11-28 05:48:49 +0900

    update

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ analyze:
 
 print:
 	cargo run -- print --printer 10.10.10.31 --tcp-data sample_tcp_data/w18_hikalium.bin
+
+analyze_all:
+	find ./sample_tcp_data/*.bin | xargs -I {} -- bash -c 'echo "*** {}" && cargo run -q -- analyze --tcp-data {} | grep -v "cmd 0x1b 0x2e"'
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![feature(new_uninit)]
+#![feature(slice_take)]
 
 use anyhow::anyhow;
 use anyhow::Context;
@@ -11,6 +12,7 @@ use std::mem::size_of;
 use std::mem::MaybeUninit;
 use std::net::TcpStream;
 use std::net::UdpSocket;
+use std::num::Wrapping;
 use std::slice;
 use std::thread;
 use std::time;
@@ -242,17 +244,43 @@ fn do_analyze(dump_file: &str) -> Result<()> {
     let label_data = fs::read(dump_file)?;
     println!("Size: {}", label_data.len());
     let mut i = 0;
+    let mut num_data_rows = 0;
     while i < label_data.len() {
         match label_data[i] {
             0x1b => match label_data[i + 1] {
                 0x7b => {
-                    let len = label_data[i + 2] as usize;
-                    println!(
-                        "cmd 0x1b 0x7b, len = {}, {:?}",
-                        len,
-                        &label_data[i + 3..i + 3 + len]
-                    );
-                    i += 3 + len;
+                    let mut payload_data = &label_data[i + 3..i + 3 + label_data[i + 2] as usize];
+                    i += 3 + payload_data.len();
+                    if payload_data.last().unwrap() != &0x7d {
+                        return Err(anyhow!(
+                            "Unexpected label data (not 0x7d): {:?}...",
+                            &label_data[i..i + 16]
+                        ));
+                    }
+                    payload_data.take_last();
+
+                    if payload_data
+                        .iter()
+                        .map(|v| Wrapping(*v))
+                        .sum::<Wrapping<u8>>()
+                        .0
+                        != payload_data.last().unwrap().wrapping_mul(2)
+                    {
+                        return Err(anyhow!(
+                            "Unexpected label data (not 0x7d): {:?}...",
+                            &label_data[i..i + 16]
+                        ));
+                    }
+                    // so the last byte of the payload_data is the checksum
+                    payload_data.take_last();
+                    if payload_data[0] == 76 {
+                        let mut tape_len_bytes = [0u8; 4];
+                        tape_len_bytes.copy_from_slice(&payload_data[1..5]);
+                        let tape_len = u32::from_le_bytes(tape_len_bytes);
+                        println!("cmd 0x1b 0x7b, {payload_data:?} tape_len = {}", tape_len);
+                    } else {
+                        println!("cmd 0x1b 0x7b, {payload_data:?}");
+                    }
                 }
                 0x2e => {
                     if label_data[i + 2..i + 6] != [0, 0, 0, 1] {
@@ -270,6 +298,7 @@ fn do_analyze(dump_file: &str) -> Result<()> {
                     }
                     print!("\n");
                     i += 8 + bytes;
+                    num_data_rows += 1;
                 }
                 _ => {
                     return Err(anyhow!(
@@ -287,6 +316,7 @@ fn do_analyze(dump_file: &str) -> Result<()> {
             }
         }
     }
+    println!("num_data_rows = {}", num_data_rows);
     Ok(())
 }
 
